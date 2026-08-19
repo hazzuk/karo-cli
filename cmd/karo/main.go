65d4639030b88fd2e9110be02debbf8f778b49e3
hazzuk <git@hazz.uk>
2026-08-19 21:37:08 +0100

    replace global vars with shared config struct

--- cmd/karo/main.go
+++ cmd/karo/main.go
@@ -6,19 +6,14 @@ package main
 
 import (
 	"github.com/hazzuk/karo-cli/internal/cli"
+	"github.com/hazzuk/karo-cli/internal/config"
 	"github.com/hazzuk/karo-cli/internal/generate"
 	"github.com/hazzuk/karo-cli/internal/lint"
 )
 
-var (
-	stackGroup      string // username_scope
-	stackName       string // jellyfin
-	stackLicense    string // AGPL-3.0-only
-	stackGroupUser  string // username
-	stackGroupScope string // scope
-)
-
 func main() {
+
+	cfg := &config.Config{}
 
 	cli.GetUserInput()
 	cli.ValidateInput()
---
+++ internal/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+type Config struct {
+	StackGroup      string // username_scope
+	StackName       string // jellyfin
+	StackLicense    string // AGPL-3.0-only
+	StackGroupUser  string // username
+	StackGroupScope string // scope
+}
